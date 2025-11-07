5e1e2e2b9ab5bd83f86ac659a1dd70cbe8524058
int-sonu <sonusebastian2025@mca.ajce.in>
2025-11-07 19:34:24 +0530

    done

--- db/db.js
+++ db/db.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import mongoose from "mongoose";
-const app=express()
+const app = express()
 
 export const connectDB = async () => {
-const url='mongodb://127.0.0.1:27017/nodeproject'
-
-try {
-  await mongoose.connect(url);
-  console.log("Mongoose successfully connected");
-} catch (err) {
-  console.error("Mongoose connection failed:");
-}
+  const url = process.env.atlas_URL
+  // console.log(url)
+  try {
+    await mongoose.connect(url);
+    console.log("Mongoose successfully connected");
+  } catch (err) {
+    console.error("Mongoose connection failed:");
+  }
 }
